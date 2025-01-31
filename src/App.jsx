50992bdeeb5710c3bb8982dce828751d5f090997
Dfcg117 <dcifuentes919@gmail.com>
2025-01-31 11:32:07 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,27 @@ function App() {
 
   const {data, request, pending, error} = useFetchApi()
   const [search, setSearch] = useState(randomID(126))
+
   useEffect(() => {
     request(`${baseUrl}/location/${search}`)
   }, [search])
 
+  return (
+    <div>
+      <section className="hero" style={{ backgroundImage: `url('https://i.redd.it/o6cwlzg3exk41.png')` }}>
+        <div className="hero-content">
+        </div>
+      </section>
 
+      <SearchForm setSearch={setSearch} />
 
-   return (
-  <div>
-    <section className="hero" style={{ backgroundImage: `url('https://i.redd.it/o6cwlzg3exk41.png')` }}>
-      <div className="hero-content">
-      </div>
-    </section>
-  </div>
-);   
-    
-    <SearchForm setSearch={setSearch}/>
-    </div>
-    </section>
-    {pending ? <p>Loading...</p> : (
-     data && <LocationCard location={data} /> 
+      {pending ? <p>Loading...</p> : (
+        data && <LocationCard location={data} />
       )}
-    <ResidentsList residents={data?.residents}/>
 
-   </div>
-  )
+      <ResidentsList residents={data?.residents} />
+    </div>
+  );
 }
 
-export default App
+export default App;
