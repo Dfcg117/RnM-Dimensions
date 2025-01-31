3c45761cacb744566e166e80f67365b0d383c1c1
Dfcg117 <dcifuentes919@gmail.com>
2025-01-31 12:10:21 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import LocationCard from "./components/LocationCard"
 import ResidentsList from "./components/ResidentsList"
 import SearchForm from "./components/SearchForm"
 import { randomID } from "./utils"
-import heroImage from './src/assets/images/hero.jpg';
 
 const baseUrl = 'https://rickandmortyapi.com/api'
 
 function App() {
 
-  const {data, request, pending, error} = useFetchApi()
+  const { data, request, pending, error } = useFetchApi()
   const [search, setSearch] = useState(randomID(126))
 
   useEffect(() => {
@@ -19,10 +18,11 @@ function App() {
 
   return (
     <div>
-    <section className="hero" style={{ backgroundImage: `url(${heroImage})` }}>
-      <div className="hero-content">
-      </div>
-    </section>
+    
+      <section className="hero" style={{ backgroundImage: `url('https://i.redd.it/o6cwlzg3exk41.png')` }}>
+        <div className="hero-content">
+        </div>
+      </section>
 
       <SearchForm setSearch={setSearch} />
 
